0988518ecf165b538c0793b4ec0e6c5a2bb7c245
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-01-06 20:45:27 -0300

    'startinBackEnd'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <p>URL Decode</p>
+      <div className="input-receive-text">
+        <textarea name="input" id="input" placeholder="Digite ou cole seu texto aqui"></textarea>
+      </div>
+
+      <div className="btn-sbt-txt">
+        <button className="btn">Decode</button>
+      </div>
+
+      <div className='text-result'>
+      <textarea disabled={true}></textarea>
+      </div>
     </div>
   );
 }
